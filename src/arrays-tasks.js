58954b98e24ed5ec91d4f68b44823ff81d97d65b
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-02-11 18:43:27 +0300

    feat: add generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -338,8 +338,12 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const range = {
+    length: len,
+  };
+
+  return Array.from(range, (item, num) => 2 * num + 1);
 }
 
 /**
