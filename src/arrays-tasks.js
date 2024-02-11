706b2a425131d6c4c340fd315cb26aadc4b190e2
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-02-11 22:51:45 +0300

    feat: add isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -150,8 +150,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const curLength = arr[0].length;
+  return arr.every((item) => item.length === curLength);
 }
 
 /**
